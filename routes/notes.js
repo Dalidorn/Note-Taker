48cd485fa272f01b7eca0f04dcf080f3a7721a76
Nate <fritz.nathaniel97@gmail.com>
2023-01-25 00:43:41 -0500

    adds get route in notes router

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,13 @@
 const notes = require('express').Router();
 
+//import helpers from fs util
+const {readFromFile, readAndAppend, writeToFile} = require('../helpers/fsUtils');
 
+//get
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
-
-
-
-
-
-
+//post
 
 module.exports = notes;
